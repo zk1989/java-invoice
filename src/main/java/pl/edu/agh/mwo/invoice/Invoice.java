5b1e5a2c48c78d97f8924cc7527b32d8d9862d95
zkocik <zkocik@d17-431-pc-20.ipa.ii.agh.edu.pl>
2025-02-08 12:10:35 +0100

    Add invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,23 +1,35 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private HashMap<Product, Integer> products = new HashMap<>();
+
+    public Invoice() {};
 
     public void addProduct(Product product) {
-        // TODO: implement
+        products.put(product, 0);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        if (products == null) {
+            return BigDecimal.ZERO;
+        } else {
+            BigDecimal subtotal = BigDecimal.ZERO;
+            for (HashMap.Entry<Product, Integer> entry : products.entrySet()) {
+                BigDecimal total = entry.getKey().getPrice().multiply(new BigDecimal(entry.getValue()));
+                subtotal = subtotal.add(total);
+            } return subtotal;
+        }
     }
 
     public BigDecimal getTax() {
